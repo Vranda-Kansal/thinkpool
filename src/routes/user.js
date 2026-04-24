4e54f73ca26db379f43ca6e30d46893cf8246468
Vranda-Kansal <itzvaniaggarwal12@gmail.com>
2026-04-24 15:20:11 +0530

    feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const { Connection } = require("../models/connectionRequest");
+const userModel = require("../models/user");
 
 //get all the received connection request for the user
 userRouter.get("/user/received/requests", userAuth, async (req, res) => {
@@ -79,6 +80,46 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
       message: "here is your friend list",
       data: data,
     });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// feed api-> all the users whose card can user see
+//1. user cannot see the card of himself
+//2. user cannot see the card of the persons to whom he like or already send request
+//3. user cannot see the card of the persons whom he pass or ignore
+//4. can't of the users who show interest in him and send the req
+//5. not of users who either reject or accept him
+
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 30 ? 30 : limit;
+    const skip = (page - 1) * limit;
+
+    const connectionEvrMade = await Connection.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+    const hideFromUsers = new Set();
+
+    connectionEvrMade.forEach((req) => {
+      hideFromUsers.add(req.fromUserId.toString());
+      hideFromUsers.add(req.toUserId.toString());
+    });
+
+    const feedUsers = await userModel
+      .find({
+        _id: { $nin: Array.from(hideFromUsers) },
+      })
+      .select("firstName lastName age gender about skills photoUrl")
+      .skip(skip)
+      .limit(limit);
+
+    res.json({ data: feedUsers });
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
